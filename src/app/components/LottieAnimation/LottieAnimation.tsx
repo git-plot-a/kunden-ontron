48c2536c2e825f2cbed5e21dd1151aaefa2fd65c
Yulia Arkadeva <yulia.arkadeva@plot-a.eu>
2024-10-15 16:34:51 +0200

    test animation

---
+++ src/app/components/LottieAnimation/LottieAnimation.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useEffect, useRef, useState } from 'react';
+import lottie from 'lottie-web';
+// import Lottie from 'lottie-react';
+import animation from "../../../../public/animation/Robo_email.json"
+import Image from 'next/image';
+
+const LottieAnimation = () => {
+  const lottieContainer = useRef(null);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Устанавливаем, что компонент отрендерился на клиенте
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    console.log(animation)
+    // Запускаем анимацию только на клиенте
+    if (isClient && lottieContainer.current) {
+      lottie.loadAnimation({
+        container: lottieContainer.current, // указание контейнера через useRef
+        animationData: animation, // путь к JSON
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+      });
+    }
+  }, [isClient]); // Данный хук сработает только на клиенте
+
+  if (!isClient) {
+    // Пока компонент рендерится на сервере, возвращаем null или пустой div
+    return null;
+  }
+
+  return <div style={{paddingTop: 100}}>
+  {/* <Lottie 
+        animationData="/animation/Robo_email.json"// Путь относительно папки public
+        loop={true}
+        style={{ width: 300, height: 300 }}
+      /> */}
+      <Image width={500} height={300} src="/animation/Robo_email_v001.gif" alt="ttt"/>
+    <div ref={lottieContainer} style={{ width: 300, height: 300 }}></div>
+  </div>;
+};
+
+export default LottieAnimation;
---
+++ src/app/prlatforms/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/_sections/Header/Header"
+import Footer from "../components/_sections/Footer/Footer"
+import LottieAnimation from "../components/LottieAnimation/LottieAnimation"
+
+const PlatformsPage = async () => {
+
+
+    return <>
+        <Header />
+        <LottieAnimation/>
+        <Footer />
+    </>
+}
+
+export default PlatformsPage
