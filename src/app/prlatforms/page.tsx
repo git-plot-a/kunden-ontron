aea20ab8873e2d1603a602e991bcc87552cc42f6
Yulia Arkadeva <yulia.arkadeva@plot-a.eu>
2024-10-15 17:18:32 +0200

    new test

--- src/app/prlatforms/page.tsx
+++ src/app/prlatforms/page.tsx
@@ -1,13 +1,16 @@
 import Header from "../components/_sections/Header/Header"
 import Footer from "../components/_sections/Footer/Footer"
-import LottieAnimation from "../components/LottieAnimation/LottieAnimation"
+// import LottieAnimation from "../components/LottieAnimation/LottieAnimation"
+import dynamic from "next/dynamic"
 
 const PlatformsPage = async () => {
-
+    const LottieAnimation = dynamic(() => import('../components/LottieAnimation/LottieAnimation'), {
+        ssr: false
+    });
 
     return <>
         <Header />
-        <LottieAnimation/>
+        <LottieAnimation />
         <Footer />
     </>
 }
