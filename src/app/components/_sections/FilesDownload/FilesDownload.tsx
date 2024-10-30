ebf556df11e3d2194252f0e7f2507e01ed370e22
Yulia Arkadeva <yulia.arkadeva@plot-a.eu>
2024-10-30 08:30:55 +0100

    animation in download area

--- src/app/components/_sections/FilesDownload/FilesDownload.tsx
+++ src/app/components/_sections/FilesDownload/FilesDownload.tsx
@@ -1,5 +1,5 @@
-"use client"
 import React from 'react';
+import clsx from 'clsx';
 import Image from 'next/image';
 import styles from "./fileDownload.module.scss"
 import constants from './constants';
@@ -20,18 +20,17 @@ type Props = {
 const FilesDownload: React.FC<Props> = ({ item, files }) => {
 
     return <div className={styles.container}>
-        <div className={styles.serviceInfo}>
+        <div className={clsx(styles.serviceInfo, 'animation-fade-in-top')}>
             <Image src={item.icon as string} alt={item.title} height={44} width={44} />
             <div className={styles.serviceTitle}>{item.title}</div>
         </div>
         <div className={styles.fileContainer}>
             {files.length > 0 && files.map((file, key) => {
-                return (<div className={styles.fileItem} key={key}>
+                return (<div className={clsx(styles.fileItem, 'animation-fade-in')} style={{ transitionDelay: `${key * 0.2}s` }} key={key}>
                     <div className={styles.text}>{file.title}</div>
                     <div className={styles.data}>{file.date}</div>
                     <div className={styles.loadButton}>
                         <StandartButton title={constants.DOWNLOAD_TEXT} active={false} image='/img/download.svg' classes={styles.button} link={file.link} />
-                        {/* <Button callback={callback} title={constants.DOWNLOAD_TEXT} image='/img/download.svg' classes={styles.button} /> */}
                     </div>
                 </div>)
             })}
