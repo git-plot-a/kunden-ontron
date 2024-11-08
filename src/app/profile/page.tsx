223bcc8371feb84a6a46725fd72f1b3b5f306390
Yulia Arkadeva <yulia.arkadeva@plot-a.eu>
2024-11-08 11:18:26 +0100

    small fixes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
+import dynamic from "next/dynamic"
 import { useRouter } from "next/navigation"
 import Header from "../components/_sections/Header/Header"
 import Footer from "../components/_sections/Footer/Footer"
 // import UserEditForm from "../components/_forms/UserEditForm/UserEditForm"
-import LottieAnimation from "../components/LottieAnimation/LottieAnimation"
+// import LottieAnimation from "../components/LottieAnimation/LottieAnimation"
 import sentAnimation from "../components/_sections/RequestFormSection/animations/Robo_email_v002.json"
 import questionAnimation from "../components/_sections/RequestFormSection/animations/Robo_question_v002.json"
 import technicalAnimation from "../components/_sections/RequestFormSection/animations/Robo_technical_v002.json"
@@ -14,12 +15,16 @@ import errorAnimation from "../components/_sections/RequestFormSection/animation
 // import api from "@/app/api/crud"
 import utils from "../utils"
 
+ const LottieAnimation = dynamic(() => import('../components/LottieAnimation/LottieAnimation'), {
+        ssr: false
+    });
+
 const ProfilePage = async () => {
     const router = useRouter()
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        console.log(utils.user.getToken())
+        // console.log(utils.user.getToken())
         if (!utils.user.getToken()) {
             utils.user.resetAllData()
             router.push('/login')
