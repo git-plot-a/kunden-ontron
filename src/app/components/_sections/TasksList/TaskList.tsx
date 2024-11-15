d90ae144042af63a10f6a683955987a53e8d04cf
Yulia Arkadeva <yulia.arkadeva@plot-a.eu>
2024-11-15 12:55:52 +0100

    visual dropdownlist

--- src/app/components/_sections/TasksList/TaskList.tsx
+++ src/app/components/_sections/TasksList/TaskList.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import TicketItem from "../../TicketItem/TicketItem"
 import constants from "./constants"
 import styles from "./taskList.module.scss"
+import DropDownList from "../../DropDownList/DropDownList"
 
 
 
@@ -12,13 +13,30 @@ type Props = {
 
 const TaskList: React.FC<Props> = ({ tickets, loading = true }) => {
 
+    const items: Array<DropDownList> = [
+        {
+            title: 'Nach dem Datum der Antragseinreichung',
+            value: '1'
+        },
+        {
+            title: 'Nach Priorität',
+            value: '2'
+        },
+        {
+            title: 'Eigene Anfragen zuerst ',
+            value: '1'
+        }
+    ]
+
     return <div>
         <h2 className={styles.tasksTitle}>{constants.TITLE}</h2>
         {tickets.length > 0 && (
             <div className={styles.toolsSection}>
                 <div className={styles.quantity}>{`${tickets.length} ${constants.QUANTITY_TITLE}`}</div>
+                <DropDownList items={items} handler={()=>{}} def={0} classes={styles.sortList}/>
             </div>
         )}
+
         <div className={styles.tasksContainer}>
             {
                 tickets.length > 0 ? tickets.map((ticket, key) => (
